e6c550dbd7e51d0c88de17a19f20a3bb45ddd11f
Victor <48831156+Weebtor@users.noreply.github.com>
2021-09-22 20:47:18 -0300

    Update tarea.py

--- tarea.py
+++ tarea.py
@@ -4,32 +4,37 @@
 # determine el ángulo de elevación, el Azimut y validar si hay o no visibilidad.
 
 import numpy as np
-Latitud_e = -0.68  # latitud estación terrena
-longitud_e = 146.52 # longitud estación terrena
-r_e = 100 # altura estacion terrestre
-Latitud_s = -0.68 # latitud satélite
-longitud_s = 146.52 # longitud satélite
-r_s = 500 # altura del satelite
+Latitud_e = -33.42628  # latitud estación terrena
+longitud_e = -70.56656 # longitud estación terrena
+r_e = 6371 # altura estacion terrestre
+Latitud_s = -0.03 # latitud satélite
+longitud_s = 30.93 # longitud satélite
+r_s = 35781.41 + r_e # altura del satelite
 
 if __name__ == "__main__":
 
-    Latitud_s = np.deg2rad(Latitud_s)
+    Latitud_e = np.deg2rad(Latitud_e)
     longitud_e = np.deg2rad(longitud_e)
     Latitud_s = np.deg2rad(Latitud_s)
     longitud_s = np.deg2rad(longitud_s)
 
 
-
+    # Obtener Gamma
     cosGamma = np.cos(Latitud_e)*np.cos(Latitud_s)*np.cos(longitud_s - longitud_e) + np.sin(Latitud_e)*np.sin(Latitud_s)
     gamma = np.arccos(cosGamma)
     print(f"COS(gamma): {cosGamma}")
-    print(f"gamma: {gamma}")
+    print(f"gamma: {np.rad2deg(gamma)}°")
+    
+    # Obtener angulo de elevacion
 
     cosEl = np.sin(gamma)/ np.sqrt((1 + np.power(r_e/r_s, 2) - 2*(r_e/r_s)*np.cos(gamma)))
     El = np.arccos(cosEl)
+
+    print(f"ángulo de elevación: {np.rad2deg(El)}°")
+
     d = r_s* np.sqrt((1 + np.power(r_e/r_s, 2) - 2*(r_e/r_s)*np.cos(gamma)))
-    print(f"ángulo de elevación:{El}")
     print(f"distancia: {d}")
+
     a_intemedio = np.arctan((np.tan(np.abs(longitud_s - longitud_e)/np.sin(np.abs(Latitud_e)))))
     print(f"angulo intermedio: {np.rad2deg(a_intemedio)}")
 
